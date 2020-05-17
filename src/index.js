9ab89a5f16b9d6b9a9deb960cbb606b0f4ce8ba7
wavila2 <wavila2@bancodebogota.com.co>
2020-05-17 12:05:05 -0500

    Formulario

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Formaulario from './components/Formulario';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Formaulario />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
