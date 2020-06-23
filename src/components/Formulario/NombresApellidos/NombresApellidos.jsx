ddf7d5c6399a7379bf57edfa670da40c9f653f15
wavila2 <wavila2@bancodebogota.com.co>
2020-06-23 08:48:23 -0500

    Fomik added

--- src/components/Formulario/NombresApellidos/NombresApellidos.jsx
+++ src/components/Formulario/NombresApellidos/NombresApellidos.jsx
@@ -1,41 +1,56 @@
-import React, { Fragment, useState,  } from 'react';
+import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './NombresApellidos.css';
 import { useHistory } from 'react-router-dom';
-
-
+import { useFormik } from 'formik';
 
 const NombresApellidos = () => {
 
-    const [datos, setDatos] = useState({
-        nombres: '',
-        apellidos: '',
-        celular: '',
-        correo: '',
-    });
 
- 
     const history = useHistory();
 
 
-    const handleInputChange = (event) => {
-        // console.log(event.target.name)
-        // console.log(event.target.value)
-        setDatos({
-            ...datos,
-            [event.target.name]: event.target.value
-        })
-    }
+ 
 
     const enviarDatos = (event) => {
         event.preventDefault();
-        sessionStorage.setItem("personal-info", JSON.stringify(datos));
+        sessionStorage.setItem("personal-info", JSON.stringify(formik.values));
 
         history.push('/ubicacion');
     }
- 
 
-    
+   const validate = (event)=> {
+        let errors = {}
+        const values = formik.values;
+        if(!values.nombres){
+            errors.nombres = 'requerido'
+        }
+        if(!values.apellidos){
+            errors.apellidos = 'requerido'
+        }
+        if(!values.celular){
+            errors.celular = 'requerido'
+        }
+        if(!values.correo){
+            errors.correo = 'requerido'
+           
+        }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+          }
+        return errors;
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            nombres: '',
+            apellidos: '',
+            celular: '',
+            correo: ''
+        },
+        validate 
+      
+    })
+    console.log("Formik Errors", formik.errors)
 
     return (<Fragment>
         <center><h2>Bienvenido para iniciar ingresa tus datos personales</h2></center>
@@ -43,29 +58,29 @@ const NombresApellidos = () => {
         <div className="center">
             <form onSubmit={enviarDatos}>
                 <div className="form-group row">
-                    <label class="col-sm-2 col-form-label">Nombres</label>
+                    <label className="col-sm-2 col-form-label">Nombres</label>
                     <div className="col-md-7">
-                        <input type="text" placeholder="Ingresa tus nombres" className="form-control" onChange={handleInputChange} name="nombres"></input>
+                        <input type="text" placeholder="Ingresa tus nombres" className="form-control" onChange={formik.handleChange} value={formik.values.nombres} name="nombres"></input>
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label class="col-sm-2 col-form-label">Apellidos</label>
+                    <label className="col-sm-2 col-form-label">Apellidos</label>
                     <div className="col-md-7">
-                        <input type="text" placeholder="Ingresa tus Apellidos" className="form-control" onChange={handleInputChange} name="apellidos"></input>
+                        <input type="text" placeholder="Ingresa tus Apellidos" className="form-control" onChange={formik.handleChange} value={formik.values.apellidos} name="apellidos"></input>
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label class="col-sm-2 col-form-label"> Celular</label>
+                    <label className="col-sm-2 col-form-label"> Celular</label>
                     <div className="col-md-7">
-                        <input type="text" placeholder="Ingresa tu numero Celular" className="form-control" onChange={handleInputChange} name="celular"></input>
+                        <input type="text" placeholder="Ingresa tu numero Celular" className="form-control" onChange={formik.handleChange} value={formik.values.celular} name="celular"></input>
                     </div>
                 </div>
                 <div className="form-group row">
-                    <label class="col-sm-2 col-form-label">Correo</label>
+                    <label className="col-sm-2 col-form-label">Correo</label>
                     <div className="col-md-7">
-                        <input type="text" placeholder="Ingresa tu correo electronico" className="form-control" onChange={handleInputChange} name="correo"></input>
+                        <input type="text" placeholder="Ingresa tu correo electronico" className="form-control" onChange={formik.handleChange} value={formik.values.correo} name="correo"></input>
                     </div>
-                </div> 
+                </div>
                 <button type="submit" className="btn btn-primary">Continuar</button>
             </form>
         </div>
