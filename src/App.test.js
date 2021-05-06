337d13ee6e26bccfb29b49fd80fe6dbe7985b9f8
William Avila <willocoav@example.com>
2021-05-06 00:43:35 -0500

    unit testing success

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { expect } from 'chai';
+import { shallow,configure } from 'enzyme';
+// import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('<App component />', () => {
+  it('renders learn react link', () => {
+       const wrapper = shallow(<App />);
+  });
 });
